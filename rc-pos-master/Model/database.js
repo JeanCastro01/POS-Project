681a8f00598a2bf6fd1c76e6c05b8854a5da9514
dloppes <d.loppes.soares@gmail.com>
2021-05-12 10:22:23 +0100

    removing js packages and dependencies - ill be using php instead

--- rc-pos-master/Model/database.js
+++ rc-pos-master/Model/database.js
@@ -19,21 +19,4 @@ db.connect((err)=>{
     console.log("Mysql Connected");
 })
 
-// const pool = createPool({
-//     host: "bscy3-pos-database.cwaccadorpob.eu-west-1.rds.amazonaws.com",
-//     user: "cct",
-//     password: "Pass1234!",
-//     database:"POS",
-//     connectionLimit:15
-// });
-
-// pool.query("select * from Customer", (error, result, fields)=>{
-// if(error){
-//     return console.log(error);
-// }
-// return console.log(result);
-// });
-
-
-
 module.exports = db;
